69f7f8005dcdf2b1e033b060c81336f5b752f209
iths-lisa-beverborg <54471572+iths-lisa-beverborg@users.noreply.github.com>
2020-08-17 16:37:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+/*class App extends React.Component {
+    render() {
+        return <div>Hello, World!</div>;
+    }
+}
+*/
+
+function App() {
+    return <div>Hello, world!</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
